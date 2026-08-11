65295dc522516ebe3e6b4cfe64c934240a715850
M-Ubaid278 <ubaid89685@gmail.com>
2026-08-11 14:05:01 +0500

    Practice of the basic concepts from start to OOP

--- prac.py
+++ prac.py
@@ -1,60 +1,71 @@
-class book:
-     def __init__(self, book_id, title, author, available):
-          self.book_id=book_id
-          self.title=title
-          self.author=author
-          self.available=available
+# class book:
+#      def __init__(self, book_id, title, author, available):
+#           self.book_id=book_id
+#           self.title=title
+#           self.author=author
+#           self.available=available
      
-     def show_details(self):
+#      def show_details(self):
         
-          print("Book_id:",self.book_id)
-          print("Title:", self.title)
-          print("Author:",self.author)
-          print("Available:",self.available,"\n")
+#           print("Book_id:",self.book_id)
+#           print("Title:", self.title)
+#           print("Author:",self.author)
+#           print("Available:",self.available,"\n")
 
 
-class library:
-     def __init__(self ):
-          self.library_Name="XYZ"
-          self.books=[]
+# class library:
+#      def __init__(self ):
+#           self.library_Name="XYZ"
+#           self.books=[]
 
-     def add_books(self,book):
-          self.books.append(book)
+#      def add_books(self,book):
+#           self.books.append(book)
 
-     def show_books(self):
-          for book in self.books:
-            book.show_details()
+#      def show_books(self):
+#           for book in self.books:
+#             book.show_details()
 
-     def borrow_book(self,Book_id):
-          for book in self.books:
-               if(book.book_id==Book_id):
-                if(book.available=='True'):
-                    book.available=False
-                    print("Book borrowed successfully")
-                else:
-                    print("Book is not available")
-               else:
-                   print("Book_id is not a valid:try again")
-
-
+#      def borrow_book(self,Book_id):
+#           for book in self.books:
+#                if(book.book_id==Book_id):
+#                 if(book.available=='True'):
+#                     book.available=False
+#                     print("Book borrowed successfully")
+#                 else:
+#                     print("Book is not available")
+#                else:
+#                    print("Book_id is not a valid:try again")
 
 
-lib=library()
 
-while True:
-     book_id=str(input("Enter your book id:"))
-     title=str(input("Enter title:"))
-     author=str(input("Enter author:"))
-     available=bool(input("Enter the availibility:"))
 
-     b=book(book_id,title,author,available)
+# lib=library()
+
+# while True:
+#      book_id=str(input("Enter your book id:"))
+#      title=str(input("Enter title:"))
+#      author=str(input("Enter author:"))
+#      available=bool(input("Enter the availibility:"))
+
+#      b=book(book_id,title,author,available)
     
-     lib.add_books(b)
+#      lib.add_books(b)
      
-     check=str(input("Do you want to enter more book:y/n  :"))
-     if(check=="n"):
-          break
-print("\n --------BOOK DETAILS----------")
+#      check=str(input("Do you want to enter more book:y/n  :"))
+#      if(check=="n"):
+#           break
+# print("\n --------BOOK DETAILS----------")
 
-lib.show_books()
-lib.borrow_book("B102")          
+# lib.show_books()
+# lib.borrow_book("B102")          
+
+list1=[1,2,3,4,5]
+list2=[5,6,7,8,9]
+
+new_list=[]
+
+for i in range(0,len(list1)):
+    x=list1[i] * list2[i]
+    new_list.append(x)
+
+print(new_list)
